790d3020c3307dc2b644d0089b6e9b390629a8eb
Vitalii Palkin <vitalijpalkin@yandex.ru>
2023-03-08 17:45:42 +0300

    burger1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__nav');
+
+burger.addEventListener('click',
+
+function() {
+
+	burger.classList.toggle('burger--active');
+
+	menu.classList.toggle('header__nav--active');
+})
